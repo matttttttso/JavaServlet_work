019d4260205a6139768403f4808282692d809fc1
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-22 10:25:04 +0900

    Create bean/Department.java

---
+++ p14_work/src/bean/Department.java
@@ -0,0 +1,31 @@
+package bean;
+
+import java.io.Serializable;
+
+public class Department implements Serializable{
+	private int deptID;
+	private String deptName;
+
+	public Department(){}
+
+	public Department(int deptID, String deptName) {
+		this.deptID = deptID;
+		this.deptName = deptName;
+	}
+
+	public int getDeptID() {
+		return deptID;
+	}
+
+	public void setDeptID(int deptID) {
+		this.deptID = deptID;
+	}
+
+	public String getDeptName() {
+		return deptName;
+	}
+
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+}
