85d51599d7c6b657f4af95c820856c4e05e99c06
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-22 14:44:04 +0900

    Create EmployeeListLogic.java

---
+++ p14_work/src/action/EmployeeListLogic.java
@@ -0,0 +1,39 @@
+package action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Employee;
+
+public class EmployeeListLogic implements CommonLogic {
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+
+		List<Employee> employees = new ArrayList<Employee>();
+		Employee e1 = new Employee(1, "山田", "太郎", 30, "男");
+		Employee e2 = new Employee(2, "佐藤", "花子", 25, "女");
+		Employee e51 = new Employee(51, "鈴木", "一朗", 46, "男");
+		employees.add(e1);
+		employees.add(e2);
+		employees.add(e51);
+		session.setAttribute("empList", employees);
+
+		@SuppressWarnings("unchecked")
+		List<Employee> empList = (List<Employee>) session.getAttribute("empList");
+
+		//あとでDAOと一緒に実装
+		//		EmployeeDAO dao = new EmployeeDAO();
+		//		List<Employee> empList = dao.findAll();
+		request.setAttribute("empList", empList);
+		if (empList == null) {
+			request.setAttribute("message", "社員の登録データがありません。");
+		}
+		return "emp_list.jsp";
+
+	}
+}
