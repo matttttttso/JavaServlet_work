cb00f495ec18d84acf0d77dec0ad769da4d1a152
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-25 16:22:39 +0900

    Update Employee.java Adding all parameter

--- p14_work/src/bean/Employee.java
+++ p14_work/src/bean/Employee.java
@@ -1,69 +1,65 @@
 package bean;
 
+import java.awt.Image;
 import java.io.Serializable;
 
 public class Employee implements Serializable {
 	private static final long serialVersionUID = 1L;
+
 	private int employeeID; //社員番号
 	private String name; //名前
 	private int age; //年齢
 	private Gender gender; //性別
+	private Image image;
+	private String zipcode;
 	private Prefecture pref; //都道府県
+	private String address;
+	private String department;
+	private String dateEntering;
+	private String dateRetired;
 
 	public Employee() {
 	}
 
-	public Employee(int employeeID, String name, int age, Gender gender) {
-		this.employeeID = employeeID;
-		this.name = name;
-		this.age = age;
-		this.gender = gender;
-	}
-	public Employee(int employeeID, String name, int age, Gender gender, Prefecture pref) {
-		this.employeeID = employeeID;
+	public Employee(int id, String name, int age, Gender gender, Prefecture pref, String dept) {
+		this.employeeID = id;
 		this.name = name;
 		this.age = age;
 		this.gender = gender;
 		this.pref = pref;
+		this.department = dept;
 	}
 
-	public int getEmployeeID() {
-		return employeeID;
-	}
+	public int getEmployeeID() {return employeeID;}
+	public void setEmployeeID(int employeeID) {this.employeeID = employeeID;}
 
-	public void setEmployeeID(int employeeID) {
-		this.employeeID = employeeID;
-	}
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name;}
 
-	public String getName() {
-		return name;
-	}
+	public int getAge() {return age;}
+	public void setAge(int age) {this.age = age;}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+	public Gender getGender() {return gender;}
+	public void setGender(Gender gender) {this.gender = gender;}
 
-	public int getAge() {
-		return age;
-	}
+	public Image getImage() {return image;}
+	public void setImage(Image image) {this.image = image;}
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+	public String getZipcode() {return zipcode;}
+	public void setZipcode(String zipcode) {this.zipcode = zipcode;}
 
-	public Gender getGender() {
-		return gender;
-	}
+	public Prefecture getPref() {return pref;}
+	public void setPref(Prefecture pref) {this.pref = pref;}
 
-	public void setGender(Gender gender) {
-		this.gender = gender;
-	}
+	public String getAddress() {return address;}
+	public void setAddress(String address) {this.address = address;}
 
-	public Prefecture getPref() {
-		return pref;
-	}
+	public String getDepartment() {return department;}
+	public void setDepartment(String department) {this.department = department;}
 
-	public void setPref(Prefecture pref) {
-		this.pref = pref;
-	}
+	public String getDateEntering() {return dateEntering;}
+	public void setDateEntering(String dateEntering) {this.dateEntering = dateEntering;}
+
+	public String getDateRetired() {return dateRetired;}
+	public void setDateRetired(String dateRetired) {this.dateRetired = dateRetired;}
 }
