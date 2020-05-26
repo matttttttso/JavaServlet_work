50dbbfc123dfc5ce60aafecebc580239e422ff70
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-27 02:54:33 +0900

    Update Picture.java

--- p14_work/src/bean/Picture.java
+++ p14_work/src/bean/Picture.java
@@ -1,18 +1,22 @@
 package bean;
 
-import java.awt.Image;
 import java.io.Serializable;
 
 public class Picture implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private int pictID;
-	private Image image;
+	private String pictImage;
 
 	public Picture(){}
 
-	public Picture(int pictID, Image image) {
+	public Picture(int pictID, String pictImage) {
 		this.pictID = pictID;
-		this.image = image;
+		this.pictImage = pictImage;
 	}
 
+	public int getPictID() {return pictID;}
+	public void setPictID(int pictID) {this.pictID = pictID;}
+
+	public String getPictImage() {return pictImage;}
+	public void setPictImage(String pictImage) {this.pictImage = pictImage;}
 }
