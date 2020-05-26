2b27be9e1cf1212505193a2dd429826c74379221
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-26 16:48:36 +0900

    Function Delete employee

---
+++ p14_work/src/action/EmpDeleteLogic.java
@@ -0,0 +1,20 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.EmployeeDAO;
+
+public class EmpDeleteLogic implements CommonLogic {
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		String empID = request.getParameter("empID");
+		EmployeeDAO empDao = new EmployeeDAO();
+		if (empDao.deleteEmp(empID) == false) {
+			request.setAttribute("errotMessage", "レコードの削除に失敗しました。");
+			return "error.jsp";
+		}
+		request.setAttribute("message", "レコードの削除に成功しました。");
+		return "success.jsp";
+	}
+}
