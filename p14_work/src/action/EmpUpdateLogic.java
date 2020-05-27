e588824e9261f48a4a5bcb7b4543323be07fdcb6
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-05-27 10:21:16 +0900

    Update EmpUpdateLogic.java (update image)

--- p14_work/src/action/EmpUpdateLogic.java
+++ p14_work/src/action/EmpUpdateLogic.java
@@ -8,44 +8,21 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import bean.Picture;
 import dao.EmployeeDAO;
 import dao.ImageDAO;
 
 public class EmpUpdateLogic implements CommonLogic {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		HttpSession session = request.getSession();
-		@SuppressWarnings("unchecked")
-		List<Picture> imageAllList = (List<Picture>) session.getAttribute("imageAllList");
-		String f = request.getParameter("picture");
-		int pictID = Integer.parseInt(request.getParameter("pictID"));
-		System.out.println(pictID);
-		if(pictID == 0) {
-			pictID = imageAllList.size() + 1;
-		}
-		System.out.println(pictID);
-		InputStream is = null;
-		try {
-			is = new FileInputStream(f);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		ImageDAO imageDAO = new ImageDAO();
-		if (imageDAO.updateImage(pictID, is) == false) {
-			request.setAttribute("errotMessage", "データベースへの登録に失敗しました。（画像データ）");
-			return "error.jsp";
-		}
-
 		boolean empIDisNull = request.getParameter("empID").equals("");
 		boolean empNameisNull = request.getParameter("empName").equals("");
 		if(empIDisNull || empNameisNull) {
-			request.setAttribute("errotMessage", "IDと名前は必須です。");
+			request.setAttribute("errorMessage", "IDと名前は必須です。");
 			return "error.jsp";
 		}
-
+		String empIDstr = request.getParameter("empID");
+		int pictID = Integer.parseInt(empIDstr);
 		List<String> empParams = new ArrayList<String>();
 		empParams.add(request.getParameter("empName"));
 		empParams.add(request.getParameter("age"));
@@ -57,12 +34,34 @@ public class EmpUpdateLogic implements CommonLogic {
 		empParams.add(request.getParameter("deptID"));
 		empParams.add(request.getParameter("dateEntering"));
 		empParams.add(request.getParameter("dateRetired"));
-		empParams.add(request.getParameter("empID"));
+		empParams.add(empIDstr);
 		EmployeeDAO empDao = new EmployeeDAO();
 		if (empDao.updateEmp(empParams) == false) {
-			request.setAttribute("errotMessage", "データベースへの登録に失敗しました。");
+			request.setAttribute("errorMessage", "データベースへの登録に失敗しました。(EMP)");
 			return "error.jsp";
 		}
+
+		String f = request.getParameter("picture");
+		InputStream is = null;
+		try {
+			is = new FileInputStream(f);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		ImageDAO imageDAO = new ImageDAO();
+		String pictureSTR = request.getParameter("pictureSTR");
+		if(pictureSTR == null) {
+			if (imageDAO.addImage(pictID, is) == false) {
+				request.setAttribute("errorMessage", "データベースへの登録に失敗しました。（画像データadd）");
+				return "error.jsp";
+			}
+		} else {
+			if (imageDAO.updateImage(pictID, is) == false) {
+				request.setAttribute("errorMessage", "データベースへの登録に失敗しました。（画像データupdate）");
+				return "error.jsp";
+			}
+		}
+
 		request.setAttribute("message", "データベースへの登録に成功しました。");
 		return "success.jsp";
 	}
