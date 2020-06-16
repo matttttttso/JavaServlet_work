c6adf86af55d26811b95d3477bbcddfb54128cf5
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-06-16 19:23:54 +0900

    DeptName validation (in Add, Not Empty)

--- p14_work/src/action/DeptAddLogic.java
+++ p14_work/src/action/DeptAddLogic.java
@@ -10,10 +10,14 @@ public class DeptAddLogic implements CommonLogic {
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 		String deptID = request.getParameter("deptID");
 		if (!deptID.matches("[1-9]|[1-9][0-9]")) {
-			request.setAttribute("errorMessage", "部署IDは1～99で入力してください");
+			request.setAttribute("errorMessage", "部署IDは1～99で入力してください。");
 			return "error.jsp";
 		}
 		String deptName = request.getParameter("deptName");
+		if (deptName.equals("")) {
+			request.setAttribute("errorMessage", "部署名は空で登録できません。");
+			return "error.jsp";
+		}
 		DeptDAO deptDao = new DeptDAO();
 		if (deptDao.addDept(deptID, deptName) == false) {
 			request.setAttribute("errorMessage", "データベースへの登録に失敗しました。");
