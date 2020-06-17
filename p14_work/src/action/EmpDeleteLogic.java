bd2cb56370fa014058fb365a206d5091bac6315b
matttttttso <48606684+matttttttso@users.noreply.github.com>
2020-06-17 11:35:21 +0900

    Change Employee delete logic

--- p14_work/src/action/EmpDeleteLogic.java
+++ p14_work/src/action/EmpDeleteLogic.java
@@ -1,27 +1,46 @@
 package action;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Picture;
 import dao.EmployeeDAO;
 import dao.ImageDAO;
 
 public class EmpDeleteLogic implements CommonLogic {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
 		String empID = request.getParameter("empID");
 		EmployeeDAO empDao = new EmployeeDAO();
-		if (empDao.deleteEmp(empID) == false) {
+		if (empDao.deleteEmp(empID) == true) {
+			ImageDAO imageDao = new ImageDAO();
+			List<Picture> imageAllList = imageDao.findAllImage();
+			session.setAttribute("imageAllList", imageAllList);
+			int pictID = Integer.parseInt(empID);
+			if (isImageContain(imageAllList, pictID)) {
+				if (imageDao.deleteImage(pictID) == false) {
+					request.setAttribute("errorMessage", "レコードの削除に失敗しました。(image)");
+					return "error.jsp";
+				}
+			}
+		} else if (empDao.deleteEmp(empID) == false) {
 			request.setAttribute("errorMessage", "レコードの削除に失敗しました。(emp)");
-			return "error.jsp";
-		}
-		ImageDAO imageDao = new ImageDAO();
-		int pictID = Integer.parseInt(empID);
-		if (imageDao.deleteImage(pictID) == false) {
-			request.setAttribute("errorMessage", "レコードの削除に失敗しました。(image)");
 			return "error.jsp";
 		}
 		request.setAttribute("message", "レコードの削除に成功しました。");
 		return "success.jsp";
+	}
+
+	boolean isImageContain(List<Picture> imageAllList, int pictID) {
+		for(Picture pict : imageAllList) {
+			if(pict.getPictID() == pictID) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
